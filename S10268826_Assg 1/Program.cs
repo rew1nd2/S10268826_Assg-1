af0a832bdf78ca9d1a9fc2354be19026c87b0371
Kiefer Wang <s10268570@connect.np.edu.sg>
2026-02-04 16:37:44 +0800

    test

---
+++ S10268826_Assg 1/Program.cs
@@ -0,0 +1,14 @@
+//==========================================================
+// Student Number : S10268826F
+// Student Name : Cyrus Tan
+// Partner Name : Kiefer Wang
+//==========================================================
+
+//Basic feature 1
+//Basic feature 2
+//Basic feature 3
+//Basic feature 4
+//Basic feature 5
+//Basic feature 6
+//Basic feature 7
+//Basic feature 8
